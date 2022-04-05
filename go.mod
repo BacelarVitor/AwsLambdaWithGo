dc752f1de1fa24f76d972ce1fcc315d4045fc835
Vitor Bacelar de Paula Augusto <bacelar@Vitors-MacBook-Pro.local>
2022-04-05 20:25:40 -0300

    create handler for the event

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/BacelarVitor/AwsLambdaWithGo
 
 go 1.18
+
+require github.com/aws/aws-lambda-go v1.29.0
--- main.go
+++ main.go
@@ -1,3 +1,24 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type MyEvent struct {
+	Name string `json:"what is your name?"`
+	Age  int    `json:"How old are you?"`
+}
+
+type MyReponse struct {
+	Message string `json:"Answer:"`
+}
+
+func HandleLambdaEvent(event MyEvent) (MyReponse, error) {
+	return MyReponse{Message: fmt.Sprintf("%s is %d years old!", event.Name, event.Age)}, nil
+}
+
+func main() {
+	lambda.Start(HandleLambdaEvent)
+}
